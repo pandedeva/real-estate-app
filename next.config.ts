a1e9bb33f78dbb0c3c9e737e9d22fa3ee729bc83
Pande Deva <devapande66@gmail.com>
2024-12-23 22:11:51 +0800

    complete home and about page

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "53.fs1.hubspotusercontent-na1.net",
+        port: "",
+        pathname: "**",
+      },
     ],
   },
 };
---
+++ src/app/about/page.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About DV Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae hic
+        deserunt ea delectus porro voluptas natus ipsam. Molestiae aliquid
+        quisquam iste voluptate corporis corrupti magnam illo in eaque ullam
+        dolor ipsum voluptates optio quasi obcaecati maiores, deleniti ut
+        dignissimos error temporibus illum rerum accusantium necessitatibus
+        tempora. Eveniet deleniti voluptates facere!
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur,
+        tempora accusamus. Distinctio ipsum ullam magnam ex. Officiis ut, fugit
+        quia quisquam, dolorum quae minima voluptatibus porro non rem, veritatis
+        expedita! Dolorum suscipit nesciunt, officia voluptas autem, minus quod
+        ex laboriosam, nemo tempore dicta libero.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit possimus
+        nostrum minus tempore nobis ea voluptas dolor vero hic repellat porro
+        inventore aspernatur numquam placeat atque reiciendis reprehenderit
+        cupiditate eos quas perferendis mollitia quibusdam, natus perspiciatis
+        praesentium. Enim rerum magnam ipsa, nulla quod architecto et cupiditate
+        magni pariatur excepturi earum veniam in incidunt fugit, reiciendis
+        mollitia voluptates corporis facere. Nemo!
+      </p>
+    </div>
+  );
+};
+
+export default About;
